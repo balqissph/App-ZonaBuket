b3fd181d626a275656a16dbc00f440390bfb6bc7
balqissph <balqissph@gmail.com>
2026-04-02 08:33:00 +0700

    Perancangan Desain

--- app/src/main/java/com/ppl3/appzonabuket/MainActivity.kt
+++ app/src/main/java/com/ppl3/appzonabuket/MainActivity.kt
@@ -5,12 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerProduct: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        recyclerProduct = findViewById(R.id.recyclerProduct)
+
+        // RecyclerView scroll horizontal
+        recyclerProduct.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
